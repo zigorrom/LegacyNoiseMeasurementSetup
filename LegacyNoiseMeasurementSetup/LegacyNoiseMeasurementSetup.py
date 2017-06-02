e681ca35e2b86fb5373a6455fcf52a5587ef3f69
zigorrom <zigorrom@gmail.com>
2017-06-02 17:36:50 +0200

    LegacyNoiseMeasurement setup

---
+++ LegacyNoiseMeasurementSetup/LegacyNoiseMeasurementSetup.py
@@ -0,0 +1,279 @@
+﻿import visa
+import serial
+from n_enum import enum
+
+def instrument_await_function(func):
+        def wrapper(self,*args,**kwargs):
+            prev_timeout = self.instrument.timeout
+            self.instrument.timeout = None
+            result = func(self,*args,**kwargs)
+            self.instrument.timeout = prev_timeout 
+            return result
+        return wrapper
+
+
+class SerialInstrument:
+    def __init__(self, resource, baud_rate = 9600):
+        self.port = serial.Serial(
+            port = resource, 
+            baud_rate = baud_rate
+            )
+
+    def isOpen(self):
+        return self.port.isOpen()
+
+    def write(self, string):
+        assert self.isOpen()
+        self.port.write(string)
+
+    def read(self):
+        assert self.isOpen()
+        return self.port.read_until()
+
+    def query(self,string):
+        assert self.isOpen()
+        self.write(string)
+        return self.read()
+
+
+   
+
+class ArduinoController(SerialInstrument):
+    def __init__(self, resource, baud_rate = 9600):
+        super().__init__(resource, baud_rate)
+
+
+
+
+
+class VisaInstrument:
+    def __init__(self, resource):
+        rm = visa.ResourceManager()
+        self.instrument = rm.open_resource(resource, write_termination = '\n',read_termination='\n')
+
+    def write(self,string):
+        assert isinstance(string, str)
+        print("writing to device: {0}".format(string))
+        self.instrument.write(string)
+
+    def query(self,string):
+        assert isinstance(string, str)
+        print("querying from device: {0}".format(string))
+        return self.instrument.ask(string)
+
+    def read(self):
+        return self.instrument.read()
+
+
+HP34401A_FUNCTIONS = enum("AVER")
+
+class HP34401A(VisaInstrument):
+    def __init__(self, resource):
+        super().__init__(resource)
+
+    def reset(self):
+        self.write("*RST")
+
+    def clear_status(self):
+        self.write("*CLS")
+
+    def query_operation_completed(self):
+        return self.query("*OPC?")
+
+    def set_nplc(self,nplc):
+        assert isinstance(nplc, float)or isinstance(nplc, int)
+        assert nplc > 0, "nplc should be greater than 0"
+        self.write("VOLT:DC:NPLC {0}".format(nplc))
+
+    def set_trigger_count(self,count):
+        assert  isinstance(count, int)
+        self.write("TRIG:COUN {0}".format(count))
+
+    def switch_beeper(self,state):
+        self.write("SYST:BEEP:STAT {0}".format("ON" if state else "OFF"))
+
+    def switch_high_ohmic_mode(self,state):
+        self.write("INP:IMP:AUTO {0}".format("ON" if state else "OFF"))
+
+    def set_function(self,func):
+        assert func in HP34401A_FUNCTIONS.indexes
+        self.write("CALC:FUNC {0}".format(HP34401A_FUNCTIONS[func]))
+
+    def switch_stat(self,state):
+        self.write("CALC:STAT {0}".format("ON" if state else "OFF"))
+
+    def switch_autorange(self,state):
+        self.write("SENS:VOLT:DC:RANG:AUTO {0}".format("ON" if state else "OFF"))
+
+    def init_instrument(self):
+        self.write("INIT")
+
+    @instrument_await_function
+    def read_voltage(self):
+        return float(self.query("MEAS:VOLTAGE:DC?")) #"CALC:AVER:AVER?"))
+
+    @instrument_await_function
+    def read_average(self):
+        return float(self.query("CALC:AVER:AVER?"))
+
+
+HP35670A_MODES = enum("FFT", "OCT", "ORD", "SINE", "HIST", "CORR")
+HP35670A_INPUTS = enum("INP1","INP2")
+HP35670A_CALC = enum("CALC1","CALC2","CALC3","CALC4")
+HP35670A_TRACES = enum("A","B","C","D")
+
+class HP3567A(VisaInstrument):
+    def __init__(self, resource):
+        super().__init__(resource)
+   
+    def initialize_instrument(self):
+        raise NotImplementedError()
+
+    def output_state(self, state):
+        self.write("OUTPUT:STAT {0}".format("ON" if state else "OFF"))
+
+    def set_source_voltage(self,voltage):
+        assert isinstance(voltage, float) or isinstance(voltage, int), "Houston, we`ve got a problem"
+        self.write("SOUR:VOLTAGE:OFFS {0}".format(voltage))
+
+    def remove_time_capture_data(self):
+        self.write(":TCAP:DEL")
+
+    def select_instrument_mode(self,mode):
+        """ mode can be FFT|OCTave|ORDer|SINE|HISTogram|CORRelation"""
+        assert mode in HP35670A_MODES.indexes
+        #print(HP35670A_MODES[mode])
+        self.write("INST:SEL {0}".format(HP35670A_MODES[mode]))
+
+    def switch_input(self,input,state):
+        assert input in HP35670A_INPUTS.indexes
+        self.write(":{0} {1}".format(HP35670A_INPUTS[input], "ON" if state else "OFF"))
+
+    def select_active_traces(self, calc, trace):
+        assert calc in HP35670A_CALC.indexes
+        assert trace in HP35670A_TRACES.indexes
+        self.write(":{0}:ACTIVE {1}".format(HP35670A_CALC[calc],HP35670A_TRACES[trace]))
+
+    def select_real_format(self, bit_number):
+        assert bit_number is 32 or bit_number is 64
+        self.write("FORM:DATA REAL, {0}".format(bit_number))
+
+    def select_ascii_format(self):
+        self.write("FORM:DATA ASC, 12")
+
+    def select_power_spectrum_function(self,calc):
+        assert calc in HP35670A_CALC.indexes
+        self.write("{0}:FEED '{1}'".format(HP35670A_CALC[calc], "XFR:POW {0}".format(calc+1)))
+
+    def select_voltage_unit(self,calc):
+        assert calc in HP35670A_CALC.indexes
+        self.write("{0}:UNIT:VOLT 'V2/Hz'".format(HP35670A_CALC[calc]))
+
+    def switch_calibration(self,state):
+        self.write("CAL:AUTO {0}".format("ON" if state else "OFF"))
+
+    def set_frequency_resolution(self,value):
+        assert isinstance(value,int)
+        self.write("FREQ:RES {0}".format(value))
+
+    def set_averaging(self,state):
+        self.write(":AVER {0}".format("ON" if state else "OFF"))
+
+    def set_display_update_rate(self, rate):
+        assert isinstance(rate,int) and rate > 0
+        self.write("SENS:AVERAGE:IRES:RATE {0}".format(rate))
+
+    def set_average_count(self,count):
+        assert isinstance(count,int) and count > 0
+        self.write("SENS:AVERAGE:COUNT {0}".format(count))
+
+    def switch_overload_rejection(self,state):
+        self.write("SENS:REJ:STAT {0}".format("ON" if state else "OFF"))
+
+    def get_points_number(self,calc):
+        assert calc in HP35670A_CALC.indexes
+        return self.query("{0}:DATA:HEAD:POIN?".format(HP35670A_CALC[calc]))
+
+    def set_frequency_start(self, value):
+        assert isinstance(value, float) or isinstance(value,int)
+        self.write("SENS:FREQ:STAR {0}".format(value))
+
+    def set_frequency_stop(self,value):
+        assert isinstance(value, float) or isinstance(value, int)
+        self.write("SENS:FREQ:STOP {0}".format(value))
+
+    @instrument_await_function
+    def get_data(self,calc):
+        assert calc in HP35670A_CALC.indexes
+        return self.query("{}:DATA?".format(HP35670A_CALC[calc]) )
+
+
+    def abort(self):
+        self.write("ABORT");
+
+    def init_instrument(self):
+        self.write(":INIT");
+
+    def clear_status(self):
+        self.write("*CLS")
+
+    @instrument_await_function
+    def calibrate(self):
+        return self.query("*CAL?")
+     
+    @instrument_await_function
+    def wait_operation_complete(self):
+        self.write("*WAI")
+
+
+ 
+if __name__== "__main__":
+
+    m1 = HP34401A("GPIB0::23::INSTR")
+    m1.clear_status()
+    m1.reset()
+    
+    m1.switch_beeper(False)
+    m1.set_nplc(0.1)
+    m1.set_trigger_count(10)
+    m1.switch_high_ohmic_mode(False)
+    m1.set_function(HP34401A_FUNCTIONS.AVER)
+    
+    m1.switch_stat(True)
+    m1.switch_autorange(True)
+    m1.init_instrument()
+    print(m1.read_average())
+    m1.switch_stat(False)
+    print(m1.read_voltage())
+    
+
+    #res = "GPIB0::6::INSTR"
+    #dev = HP3567A(res)
+    #dev.abort()
+    #dev.calibrate()
+    #dev.set_source_voltage(6.6)
+    #dev.output_state(True)
+    #time.sleep(2)
+    #dev.output_state(False)
+    #print(HP35670A_MODES.FFT)
+    #dev.select_instrument_mode(HP35670A_MODES.FFT)
+    #dev.switch_input(HP35670A_INPUTS.INP2, False)
+    #dev.select_active_traces(HP35670A_CALC.CALC1, HP35670A_TRACES.A)
+    ##dev.select_real_format(64)
+    #dev.select_ascii_format()
+    #dev.select_power_spectrum_function(HP35670A_CALC.CALC1)
+    #dev.select_voltage_unit(HP35670A_CALC.CALC1)
+    #dev.switch_calibration(False)
+    #dev.set_average_count(10)
+    #dev.set_display_update_rate(2)
+    #dev.set_frequency_resolution(1600)
+    #dev.set_frequency_start(64.0)
+    #dev.set_frequency_stop(102.4e3)
+
+    #print(dev.get_points_number(HP35670A_CALC.CALC1))
+
+
+    #dev.init_instrument()
+    #dev.wait_operation_complete()
+
+    #print(dev.get_data(HP35670A_CALC.CALC1))
