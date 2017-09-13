869920792dc9590389bf00c62dcf083bebbbad42
zigorrom <zigorrom@gmail.com>
2017-09-13 19:40:56 +0200

    123

--- LegacyNoiseMeasurementSetup/experiment_writer.py
+++ LegacyNoiseMeasurementSetup/experiment_writer.py
@@ -39,7 +39,9 @@ class ExperimentWriter():
             ["Temperature\-(E)","K"],
             ["k\-(ampl)","int"],
             ["N\-(aver)","int"],
-            ["V\-(Gate)","V"]
+            ["V\-(0Gate)","V"],
+            ["V\-(EGate)","V"],
+            ["Current\_0", "A"],
             ]).transpose()
         self._measurement_info_data = np.asarray([["Frequency","Hz"],["Sv","V2/Hz"]]).transpose()
         #    ]
@@ -97,12 +99,52 @@ class ExperimentWriter():
         if self._measurement_file and not self._measurement_file.closed:
             self._measurement_file.close()
 
+            # ["U\_sample","V"],
+            #["Current", "A"],
+            #["R\_equivalent", "Ohm"],
+            #["Filename","str"],
+            #["R\_load","Ohm"],
+            #["U\_whole","V"],
+            #["U\_0sample", "V"],
+            #["U\_0whole","V"],
+            #["R\-(0sample)","Ohm"],
+            #["R\-(Esample)","Ohm"],
+            #["Temperature\-(0)","K"],
+            #["Temperature\-(E)","K"],
+            #["k\-(ampl)","int"],
+            #["N\-(aver)","int"],
+            #["V\-(0Gate)","V"],
+            #["V\-(EGate)","V"],
+            #["Current\_0", "A"],
+
+
     def write_measurement_info(self,info):
         if not self._experiment_file:
             return 
 
         if isinstance(info, MeasurementInfo):
-            self._experiment_file.write(str(info).encode())
+            data_list = [info.end_sample_voltage,
+                         info.sample_current_end, 
+                         info.equivalent_resistance_end, 
+                         generate_measurement_info_filename(info.measurement_filename, info.measurement_count,info._measurement_file_extension),
+                         info._load_resistance,
+                         info.end_main_voltage,
+                         info.start_sample_voltage,
+                         info.start_main_voltage,
+                         info.sample_resistance_start,
+                         info.sample_resistance_end,
+                         info.start_temperature,
+                         info.end_temperature,
+                         None,
+                         None,
+                         info.start_gate_voltage,
+                         info.end_gate_voltage,
+                         info.sample_current_start
+                         ]
+            representation = "\t".join(map(str,data_list)) + '\n'
+            
+
+            self._experiment_file.write(representation.encode())
             self._experiment_file.flush()
 
     def write_measurement(self,data):
